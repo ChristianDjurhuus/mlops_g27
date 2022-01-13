1aeb50ff8c226a6b744ebde1116205bbefe1b6e3
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2022-01-13 12:52:55 +0100

    Adding model training script

---
+++ src/models/train_model.py
@@ -0,0 +1,107 @@
+from transformers import AutoTokenizer
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification
+from torch.utils.data import DataLoader
+from transformers import get_scheduler
+import torch
+from datasets import load_metric
+from transformers import AdamW
+import numpy as np
+
+
+#################################### Temporaily importing data ###########################################################
+raw_datasets = load_dataset("imdb")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000)) 
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000)) 
+full_train_dataset = tokenized_datasets["train"]
+full_eval_dataset = tokenized_datasets["test"]
+
+tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
+eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
+
+
+########################################################################################################################
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+#Implementing learning rate scheduler
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps
+)
+
+#Checking for gpu's
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model.to(device)
+
+model.train()
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    batch_length = 0.0
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+
+        running_loss += loss.item()
+        batch_length += len(batch)
+
+        print("[%d] loss: %.3f" % (epoch + 1, running_loss / batch_length))
+        running_loss = 0.0
+        
+
+        
+
+
+metric = load_metric("accuracy")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
+        
+        
+
+
+
+
+
+
+
+
+
