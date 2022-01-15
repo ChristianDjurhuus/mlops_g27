3b2cf8788a8a6dc2a29dbf70ebe8428e146dd4d3
AndreasHNielsen <andreas.hornemann@gmail.com>
2022-01-15 13:19:14 +0100

    pep8 optimized file for compliance

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,40 +1,39 @@
 import torch
 from transformers import AutoTokenizer
-from datasets import load_dataset
+from datasets import load_from_disk
 from torch.utils.data import DataLoader
 from datasets import load_metric
 from transformers import AutoModelForSequenceClassification
-
-# Temporaily importing data
-raw_datasets = load_dataset("imdb")
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+from data_path import get_data_path
 
 
-def tokenize_function(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+def fetch_data():
+    path = get_data_path("mlops_g27/data/processed")
+    processed_datasets = load_from_disk(path)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+    tokenized_datasets = processed_datasets.map(tokenize_function, batched=True)
+    tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+    tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+    tokenized_datasets.set_format("torch")
 
-small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
-small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
-full_train_dataset = tokenized_datasets["train"]
-full_eval_dataset = tokenized_datasets["test"]
-
-tokenized_datasets = tokenized_datasets.remove_columns(["text"])
-tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
-tokenized_datasets.set_format("torch")
-
-small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
-small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
-
-train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
-eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
+    small_test_dataset = (
+        tokenized_datasets["test"].shuffle(seed=1984).select(range(1000))
+    )
+    test_dataloader = DataLoader(
+        small_test_dataset, shuffle=True, batch_size=4
+    )
+    return test_dataloader
 
 
 def evaluate():
     print("Evaluating")
     metric = load_metric("accuracy")
+    # initializing wandb logging
+    test_dataloader = fetch_data()
     model = AutoModelForSequenceClassification.from_pretrained(
         "bert-base-cased", num_labels=2
     )
@@ -44,14 +43,12 @@ def evaluate():
     model.to(device)
     model.eval()
     with torch.no_grad():
-        # NEED TO DEFINE EVAL_DATALOADER
-        for batch in eval_dataloader:
+        for batch in test_dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
             logits = outputs.logits
             predictions = torch.argmax(logits, dim=-1)
             metric.add_batch(predictions=predictions, references=batch["labels"])
-
             print(f'Accuracy: {metric.compute().get("accuracy")*100}%')
 
 
